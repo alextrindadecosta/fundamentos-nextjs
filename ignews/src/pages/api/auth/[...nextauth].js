8815d49533b8e4e6e5bed7118142efc7ab2b4765
alextrindadecosta <alextrindade-costa@outlook.com>
2022-05-25 01:56:49 -0300

    configured faunadb, signin callback

--- ignews/src/pages/api/auth/[...nextauth].js
+++ ignews/src/pages/api/auth/[...nextauth].js
@@ -1,19 +1,57 @@
+import { query } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization:  {
+      authorization: {
         params: {
           scope: 'read:user',
         },
       },
     }),
-    // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user.user
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(email)
+              )
+            )
+          )
+        )
+        
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
